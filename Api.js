e56aa18b35f88fbb10f443ac989d3247efe8831a
Phil <philx_x@hotmail.com>
2016-04-09 21:00:14 -0400

    add Promise version to RC API module

--- Api.js
+++ Api.js
@@ -8,6 +8,19 @@ var Api = {
             return body
         } else {callback('api call was not successful', null)}
       })
+  },
+  getPromise: function (path, token) {
+    return new Promise(function(resolve, reject) {
+      request('https://www.recurse.com/api/v1' + path + '?access_token=' + token,
+      function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          //resolve promise when request was successful
+          resolve(body)
+        } else {
+          reject(error)
+        }
+      })
+    })
   }
 }
 module.exports = Api
